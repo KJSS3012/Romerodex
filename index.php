b69c80f6e3ae2ab06db9737c40210101d1df5279
Kaique José de Souza Santos <kaique30122004@gmail.com>
2022-05-29 15:27:21 -0300

    Homepage

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RomeroDex - Início</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class = "header-homepage">
+        <img src="_image/Logo.png" alt="RomeroDex" class ="logo-homepage">
+    </header>
+    <main class = "main-homepage">
+        <section class = "section1-homepage">
+            <div class = "div-s1-homepage">
+                <h1>Bem-Vindo a Romerodex, o melhor sistema de registro de romeros que existe em todo o mundo!</h1>
+            </div>
+        </section>
+        <section class = "section2-homepage">
+            <div class = "div-s2-homepage">
+                <input type="button" value="Histórico" class = "button-homepage">
+                <input type="button" value="Registrar" class = "button-homepage">
+                <input type="button" value="Criadores" class = "button-homepage">
+            </div>
+        </section>
+    </main>
+    <footer class = "footer-homepage">
+        <div class = "div1-f-homepage"></div>
+        <div class = "div2-f-homepage"></div>
+    </footer>
+</body>
+</html>
