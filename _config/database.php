1e515f5ba931d01646b0f66d6e0eb8078e231fdf
Kaique José de Souza Santos <kaique30122004@gmail.com>
2022-06-02 15:50:32 -0300

    Banco de dados funfando

--- _config/database.php
+++ _config/database.php
@@ -2,13 +2,10 @@
 
 $db = new SQLite3('database.db');
 
-$db->exec('CREATE TABLE IF NOT EXISTS Romeromons(NomeDoTreinador TEXT, NomeDoRomeromon TEXT, Descricao TEXT');
+$db->exec("CREATE TABLE IF NOT EXISTS Romeromons(NomeDoTreinador TEXT, NomeDoRomeromon TEXT, Descricao TEXT, ID INTEGER PRIMARY KEY)");
 
-if (isset($_GET['nt']) && isset($_GET['nr']) && isset($_GET['dc'])) {
-    $nt = $_GET['nt'];
-    $nr = $_GET['nr'];
-    $dc = $_GET['dc'];
-    // $im = $_GET['im'];
+$nt = $_GET['nt'];
+$nr = $_GET['nr'];
+$dc = $_GET['dc'];
 
-    $db->exec('INSERT INTO Romeromons ("NomeDoTreinador,NomeDoRomeromon,Descricao") values("' . $nt . ',' . $nr . ',' . $dc . '")');
-}
+$db->exec("INSERT INTO Romeromons(NomeDoTreinador, NomeDoRomeromon, Descricao) values('$nt','$nr','$dc')");
